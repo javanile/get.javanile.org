3fa2412c79fab75546eafd3d15eb43db15b932b0
Francesco Bianco <info.francescobianco@gmail.com>
2022-11-15 10:53:49 +0100

    Update bpkg.sh

--- app/bpkg.sh
+++ app/bpkg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ## Install BPKG from the official GitHub repository link
-curl -Lo- "https://raw.githubusercontent.com/bpkg/bpkg/master/setup.sh" | bash
+curl -sLo- "https://raw.githubusercontent.com/bpkg/bpkg/master/setup.sh" | bash
