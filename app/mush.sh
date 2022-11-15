860bda0c0358eedff0304c9ee48bae45ae704fd1
Francesco Bianco <info.francescobianco@gmail.com>
2022-11-15 18:01:41 +0100

    Update mush.sh

--- app/mush.sh
+++ app/mush.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
+curl=curl
+chmod=chmod
+
+if [[ $EUID -ne 0 ]]; then
+    curl="sudo ${curl}"
+    chmod="sudo ${chmod}"
+fi
+
+${curl} -sL https://raw.githubusercontent.com/javanile/mush/main/bin/mush -o /usr/local/bin/mush
+${chmod} +x /usr/local/bin/mush
+
+mush --version
