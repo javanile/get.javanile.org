8dc44d55964b51d0725c5ea70e0658481d4b3b6c
Francesco Bianco (yafb) <info.francescobianco@gmail.com>
2025-02-13 22:27:46 +0100

    Update mush.sh

--- app/mush.sh
+++ app/mush.sh
@@ -7,11 +7,12 @@ curl -s https://get.javanile.org/install | bash -s mush 0.2.0
 ## Override with development branch
 if [ "$1" = "--branch" ] && [ -n "$2" ]; then
   git_uri=https://github.com/javanile/mush.git
-  src_dir=$HOME/.mush/registry/src/https-github-com-javanile-mush/mush
+  src_dir=${HOME:-/root}/.mush/registry/src/https-github-com-javanile-mush/mush
 
   echo "Override with development branch: $2"
   mush install console
   mkdir -p "${src_dir}"
+  rm -fr "${src_dir}/$2" && true
   git clone --single-branch --branch "$2" "${git_uri}" "${src_dir}/$2"
   cd "${src_dir}/$2"
   mush build --release
